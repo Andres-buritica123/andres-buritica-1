89da5f32ff0c98969cffc4acc8ee8ce18b8bb188
Andres_Buritica <144363391+Andres-buritica123@users.noreply.github.com>
2025-06-13 17:23:53 -0500

    Update Proyecto_Integrador.py

--- pages/Proyecto_Integrador.py
+++ pages/Proyecto_Integrador.py
@@ -20,31 +20,11 @@ col1, col2, col3, col4 = st.columns(4)
 # Columna 1
 with col1:
     st.image("assets/foto.jpg", width=200, caption="Estudiante", output_format="JPEG")
-
-# Columna 2
-with col2:
     st.markdown('<h3 style="color: #0066cc; margin-top: 0px;">Andrés Felipe Buritica Ocampo</h3>', unsafe_allow_html=True)
     st.markdown('<p style="margin-top: 10px;">Programa: <span style="color: #0066cc; font-weight: bold;">Desarrollo de Software</span></p>', unsafe_allow_html=True)
-
-# Columna 3
-with col3:
-    st.markdown('<p>Semestre: <span style="color: #0066cc; font-weight: bold;">2025-1</span></p>', unsafe_allow_html=True)
-
-# Columna 4
-with col4:
-    st.markdown('<p>Repositorio: <a href="https://github.com/Andres-buritica123/andres-buritica-1.git" target="_blank" style="color: #0066cc; font-weight: bold; text-decoration: none;">GitHub</a></p>', unsafe_allow_html=True)
-
-col1, col2, col3, col4 = st.columns(4)
-
-# Columna 1
-with col1:
+# Columna 2
+with col2:
     st.image("assets/foto.jpg", width=200, caption="Estudiante", output_format="JPEG")
-
-# Columna 2
-with col2:
-    st.markdown('<h3 style="color: #0066cc; margin-top: 0px;">Andrés Felipe Buritica Ocampo</h3>', unsafe_allow_html=True)
-    st.markdown('<p style="margin-top: 10px;">Programa: <span style="color: #0066cc; font-weight: bold;">Desarrollo de Software</span></p>', unsafe_allow_html=True)
-
 # Columna 3
 with col3:
     st.markdown('<p>Semestre: <span style="color: #0066cc; font-weight: bold;">2025-1</span></p>', unsafe_allow_html=True)
@@ -52,7 +32,7 @@ with col3:
 # Columna 4
 with col4:
     st.markdown('<p>Repositorio: <a href="https://github.com/Andres-buritica123/andres-buritica-1.git" target="_blank" style="color: #0066cc; font-weight: bold; text-decoration: none;">GitHub</a></p>', unsafe_allow_html=True)
-    
+
 # -----------------------------
 # 🧩 Parte 1: Estructuras de Datos
 # -----------------------------
